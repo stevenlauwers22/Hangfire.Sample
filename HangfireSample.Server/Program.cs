392fe01d7424d21434febbadca88bf6b79506a8e
Steven Lauwers <stevenlauwers22@hotmail.com>
2017-07-26 15:18:13 +0200

    Added the hangfire server

---
+++ HangfireSample.Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Hangfire;
+
+namespace HangfireSample.Server
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireSample");
+
+            using (new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
